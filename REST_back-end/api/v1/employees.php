6971996cc821cabb559179a632e80edac7b82b3d
kissroland <kissrolandkristof@gmail.com>
2019-09-09 10:44:24 +0200

    REST back_end example

---
+++ REST_back-end/api/v1/employees.php
@@ -0,0 +1,144 @@
+<?php
+//connect to database
+include("../connection.php");
+$db = new dbObj();
+$connection =  $db->getConnstring();
+
+$request_method=$_SERVER["REQUEST_METHOD"];
+
+switch($request_method)
+	{
+	case 'GET':
+			// Retrive Products
+			if(!empty($_GET["id"]))
+			{
+				$id=intval($_GET["id"]);
+				get_employees($id);
+			}
+			else
+			{
+				get_employees();
+			}
+			break;
+
+    case 'POST':
+                // Insert Product
+                insert_employee();
+            break;
+
+    case 'PUT':
+                // Update Product
+                $id=intval($_GET["id"]);
+                update_employee($id);
+            break;
+
+    case 'DELETE':
+                // Delete Product
+                $id=intval($_GET["id"]);
+                delete_employee($id);
+            break;
+
+            default:
+			// Invalid Request Method
+			header("HTTP/1.0 405 Method Not Allowed");
+			break;
+	}
+
+// HTTP GET METHODS
+
+function get_employees()
+{
+    global $connection;
+     $query="SELECT * FROM employee";
+    $response=array();
+    $result=mysqli_query($connection, $query);
+    while($row=mysqli_fetch_array($result))
+    {
+        $response[]=$row;
+    }
+    header('Content-Type: application/json');
+    echo json_encode($response);
+}
+
+
+
+//HTTP POST METHODS
+function insert_employee()
+{
+    global $connection;
+
+    $data = json_decode(file_get_contents('php://input'), true);
+    $employee_name=$data["employee_name"];
+    $employee_salary=$data["employee_salary"];
+    $employee_age=$data["employee_age"];
+    echo $query="INSERT INTO employee SET employee_name='".$employee_name."', employee_salary='".$employee_salary."', employee_age='".$employee_age."'";
+    if(mysqli_query($connection, $query))
+    {
+        $response=array(
+            'status' => 1,
+            'status_message' =>'Employee Added Successfully.'
+        );
+    }
+    else
+    {
+        $response=array(
+            'status' => 0,
+            'status_message' =>'Employee Addition Failed.'
+        );
+    }
+    header('Content-Type: application/json');
+    echo json_encode($response);
+}
+
+
+//HTTP PUT METHODS
+
+function update_employee($id)
+{
+		global $connection;
+		$post_vars = json_decode(file_get_contents("php://input"),true);
+		$employee_name=$post_vars["employee_name"];
+		$employee_salary=$post_vars["employee_salary"];
+		$employee_age=$post_vars["employee_age"];
+		$query="UPDATE employee SET employee_name='".$employee_name."', employee_salary='".$employee_salary."', employee_age='".$employee_age."' WHERE id=".$id;
+		if(mysqli_query($connection, $query))
+        {
+            $response=array(
+                'status' => 1,
+                'status_message' =>'Employee Updated Successfully.'
+            );
+        }
+        else
+        {
+            $response=array(
+                'status' => 0,
+                'status_message' =>'Employee Updation Failed.'
+            );
+        }
+		header('Content-Type: application/json');
+		echo json_encode($response);
+	}
+
+//HTTP DELETE METHODS
+
+function delete_employee($id)
+{
+    global $connection;
+    $query="DELETE FROM employee WHERE id=".$id;
+    if(mysqli_query($connection, $query))
+    {
+        $response=array(
+            'status' => 1,
+            'status_message' =>'Employee Deleted Successfully.'
+        );
+    }
+    else
+    {
+        $response=array(
+            'status' => 0,
+            'status_message' =>'Employee Deletion Failed.'
+        );
+    }
+    header('Content-Type: application/json');
+    echo json_encode($response);
+}
