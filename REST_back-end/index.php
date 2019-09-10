e2543ca4641a7b697f7740647d889bfed0303536
kissroland <kissrolandkristof@gmail.com>
2019-09-10 13:03:16 +0200

    REST back_end index.php edit

--- REST_back-end/index.php
+++ REST_back-end/index.php
@@ -1,9 +1,22 @@
 
 <html>
     <body>
+<h2>Implement basic REST back-end</h2>
+
+<p>The GET Method </p>
 
 <form action = "api/offers.php" method = "GET">
     ID: <input type = "text" name = "id" />
+    <input type = "submit" />
+</form>
+
+
+<p>The POST Method </p>
+<form action = "api/offers.php" method = "POST">
+    ID: <input type = "text" name = "id" />
+    Owner: <input type = "text" name = "owner" />
+    From: <input type = "text" name = "from" />
+    To: <input type = "text" name = "to" />
     <input type = "submit" />
 </form>
 
