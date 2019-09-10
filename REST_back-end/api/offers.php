567c842ada61d094629cd8d0b974b43dfbf2683c
kissroland <kissrolandkristof@gmail.com>
2019-09-10 18:21:20 +0200

    REST back_end PUT method

--- REST_back-end/api/offers.php
+++ REST_back-end/api/offers.php
@@ -8,6 +8,7 @@ switch($request_method)
 			// Retrive Products
 			if(!empty($_GET["id"]))
 			{
+
 				$id=intval($_GET["id"]);
 
 
@@ -33,6 +34,9 @@ switch($request_method)
     case 'PUT':
                 // Update Product
                 $id=intval($_GET["id"]);
+                $owner=intval($_POST["owner"]);
+                $from=intval($_POST["from"]);
+                $to=intval($_POST["to"]);
                 update_offers($id);
             break;
 
@@ -64,7 +68,11 @@ function get_offers($id)
           echo "ID_one: " . $x ." : " . $x_value;
           echo "<br>";
       }
-      print_r($array);
+
+      var_dump(
+          $array,
+          json_encode($array)
+      );
 
   }elseif ($_GET["id"] == 2){
 
@@ -85,12 +93,16 @@ function get_offers($id)
 
 
 
-      print_r($array_two);
+
+      var_dump(
+          $array_two,
+          json_encode($array_two)
+      );
 
     exit();
 
     }else{
-      echo "invald ID ";
+      echo "invalid ID ";
   }
 
 }
@@ -101,10 +113,12 @@ function get_offers($id)
 //HTTP POST METHODS
 function insert_offers()
 {
+
     if( $_POST["id"] || $_POST["owner"] || $_POST["from"] || $_POST["to"]   ) {
         if (preg_match("/[^0-9-]/",$_POST['id'] )) {
             die ("invalid ID should be number");
         }
+
 
         echo "ID: " . $_POST['id'] . "<br />";
         echo "owner: " . $_POST['owner'] . "<br />";
@@ -121,6 +135,29 @@ function insert_offers()
 
 function update_offers($id)
 {
+    if ($_GET["id"] == 6 ){
+        $id = array(
+            array("id" =>"6","owner" =>"Oliver","from" =>"Szeged","to" =>"Pest"),
+            array("id" =>"7","owner" =>"mike","from" =>"kecskemet","to" =>"kerekdomb"),
+            array("id" =>"6","owner" =>"Oliver","from" =>"Szeged","to" =>"Pest"),
+            array("id" =>"6","owner" =>"Oliver","from" =>"Szeged","to" =>"Pest"),
+
+        );
+
+        echo $id[0][0].": ID: ".$id[0][1].", sold: ".$id[0][2].".<br>";
+        echo $id[1][0].": ID: ".$id[1][1].", sold: ".$id[1][2].".<br>";
+        echo $id[2][0].": ID: ".$id[2][1].", sold: ".$id[2][2].".<br>";
+        echo $id[3][0].": ID: ".$id[3][1].", sold: ".$id[3][2].".<br>";
+
+
+
+
+
+
+    }
+
+
+
 
 }
 
--- REST_back-end/index.php
+++ REST_back-end/index.php
@@ -20,5 +20,15 @@
     <input type = "submit" />
 </form>
 
+<p>The PUT Method </p>
+
+<form action = "api/offers.php" method = "GET">
+    ID: <input type = "text" name = "id" />
+    Owner: <input type = "text" name = "owner" />
+    From: <input type = "text" name = "from" />
+    To: <input type = "text" name = "to" />
+    <input type = "submit" />
+</form>
+
     </body>
 </html>
