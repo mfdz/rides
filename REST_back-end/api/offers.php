b4909055da0adf2f81a2c633e08550fb7772d5fe
Nandor Poka <np@gerhardt.io>
2019-09-11 09:06:53 +0200

    Added comments to Roland

--- REST_back-end/api/offers.php
+++ REST_back-end/api/offers.php
@@ -20,7 +20,8 @@ switch($request_method)
 			break;
 
     case 'POST':
-                // Insert Product
+               // Itt a $_GET az nem a GET POST PUT meg feleloje hanem egy PHP fuggveny ami valtozokat ker el nev alapjan a HTTP keresbol. Az eredeti peldaban midnenutt ezt hasznaltak.
+               // A PUT-ban meg a DELETE-ben sem irtak $_DELETE-t meg $_PUT-ot.....
                 if(!empty($_POST["id"] || $_POST["owner"] || $_POST["from"] || $_POST["to"])) {
                     $id=intval($_POST["id"]);
                     $owner=intval($_POST["owner"]);
@@ -52,11 +53,14 @@ switch($request_method)
 
 function get_offers($id)
 {
+    // a fuggvenynek, metodusnak nevezzuk akarhogy, mar atadod az $id valtozot. Tok felesleges es nem is helyes a $_GET["id"] hasnzalni mert mar atadjuk a fuggvenyhivaskor.active
   if ($_GET["id"] == 1) {
+      // ez jo otlet, lehetne mashogy is de jo otlet.
       if (preg_match("/[^0-9-]/",$_GET['id'] )) {
           die ("invalid ID should be number");
       }
-
+    // teljes felreertelmezese a dolgoknak. A $_GET["id"] egy konkret ertek ad vissza amit egyenlove akarsz tenni masvalamivel.
+    // ide valamilyen valtozot kell tenned es azt inicializalni valamilyen ertekkel. 
       $_GET["id"] = $array = ["owner" => "mfdz",
           "from" => "Musberg",
           "to" => "Stuttgart"];
@@ -67,7 +71,7 @@ function get_offers($id)
       print_r($array);
 
   }elseif ($_GET["id"] == 2){
-
+// lasd elozo kommentem
       $_GET["id"] = $array_two = ["owner" => "mfdz",
           "from" => "Stuttgart",
           "to" => "Musberg"];
@@ -99,8 +103,10 @@ function get_offers($id)
 
 
 //HTTP POST METHODS
+//a 30. sorban parameterekkel van meghivva a fuggveny. Ez itt egy parameterek nelkuli fuggveny implementacio. ebben a formaban sosem lesz meghivva.
 function insert_offers()
 {
+    // lasd a get offers($id)=nel levo kommentemet. a jo megoldas az hogy atadjuk a fuggvenynek a parametereket es azokat hasznaljuk
     if( $_POST["id"] || $_POST["owner"] || $_POST["from"] || $_POST["to"]   ) {
         if (preg_match("/[^0-9-]/",$_POST['id'] )) {
             die ("invalid ID should be number");
